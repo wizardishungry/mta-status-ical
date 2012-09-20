919b779507b28a438c9248234513ca57bb01766b
Jon Williams <jon.williams@hollywood.com>
2012-09-20 11:37:58 -0400

    Monkeypatching rubyredrick/ri_cal#5 to fix X-Properties

--- lib/mta-status-ical.rb
+++ lib/mta-status-ical.rb
@@ -13,6 +13,16 @@ class String
   end
 end
 
+class RiCal::Component::Calendar
+  def export_x_properties_to(export_stream) #:nodoc:
+    x_properties.each do |name, props|
+	  props.each do | prop |
+		export_stream.puts("#{name}#{prop}")
+	  end
+	end
+  end
+end
+
 def douchebag(name)
     name =~ /\s|[a-z]|SIR/
 end
