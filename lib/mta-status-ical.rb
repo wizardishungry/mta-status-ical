5660534d0fc0c46f9f920b631d8584849ae74d81
Jon Williams <jonathan.williams@gmail.com>
2018-03-07 13:25:23 -0500

    shovel this drivel into a class

--- lib/mta-status-ical.rb
+++ lib/mta-status-ical.rb
@@ -16,74 +16,78 @@ end
 class RiCal::Component::Calendar
   def export_x_properties_to(export_stream) #:nodoc:
     x_properties.each do |name, props|
-			props.each do | prop |
-				export_stream.puts("#{name}#{prop}")
-			end
-		end
+      props.each do | prop |
+        export_stream.puts("#{name}#{prop}")
+      end
+    end
   end
 end
 
-def douchebag(name)
-    name =~ /\s|[a-z]|SIR/
-end
-
-def bus(name)
-    name =~ / - /
-end
-
-doc = Nokogiri::XML(open "http://www.mta.info/status/serviceStatus.txt")
-
-RiCal::PropertyValue::DateTime::default_tzid = 'America/New_York'
-cal = RiCal.Calendar do
-    add_x_property 'X-WR-CALNAME', 'MTA Subway Delays'
-    add_x_property 'X-PUBLISHED-TTL', 'PT1M'
-end
-
-doc.css("line").map do |line|
-  name =  line.css("name").first.content
-  status = line.css("status").first.content
-  text = Sanitize.clean( line.css("text").first.content ).strip
-
-  dt = line.css('Date').first.content + " " + line.css('Time').first.content
-  #puts "DEBUG #{name} #{status} #{dt}"
-  dt.strip!
-
-  def dt_end_offset(dt)
-		if dt.hour >= 12 or DateTime.now-dt > 1.0
-			2
-		else
-			1
-		end
+class MtaStatusIcal
+  def douchebag(name)
+      name =~ /\s|[a-z]|SIR/
   end
 
-  if dt == ''
-		dt = DateTime.parse(Time.now.to_s).to_date
-		dtend = (DateTime.now + dt_end_offset(DateTime.now)).to_date
-  else
-		dt = DateTime.parse(dt).to_datetime
-		dtend = DateTime.parse( (DateTime.now + dt_end_offset(dt) ).to_date.to_s )
+  def bus(name)
+      name =~ / - /
   end
 
-	if text =~ /until ([[:alpha:]]{3} \d+)/
-		#puts "FF #{$1}"
-	end
+  def run
 
-  if status != "GOOD SERVICE" and not douchebag name and not bus name
-		cal.events << RiCal.Event do
-			summary "#{name} #{status.titleize}"
-			dtstart     dt
-			dtend       dtend
-			location    name
-			description text
+    doc = Nokogiri::XML(open "http://web.mta.info/status/serviceStatus.txt")
 
-			if not status =~ /Planned|Good/i
-				alarm do
-					description "#{name} #{status.titleize}"
-				end
-			end
+    RiCal::PropertyValue::DateTime::default_tzid = 'America/New_York'
+    cal = RiCal.Calendar do
+        add_x_property 'X-WR-CALNAME', 'MTA Subway Delays'
+        add_x_property 'X-PUBLISHED-TTL', 'PT1M'
+    end
 
-		end
-	end
+    doc.css("line").map do |line|
+      name =  line.css("name").first.content
+      status = line.css("status").first.content
+      text = Sanitize.clean( line.css("text").first.content ).strip
+
+      dt = line.css('Date').first.content + " " + line.css('Time').first.content
+      #puts "DEBUG #{name} #{status} #{dt}"
+      dt.strip!
+
+      def dt_end_offset(dt)
+        if dt.hour >= 12 or DateTime.now-dt > 1.0
+          2
+        else
+          1
+        end
+      end
+
+      if dt == ''
+        dt = DateTime.parse(Time.now.to_s).to_date
+        dtend = (DateTime.now + dt_end_offset(DateTime.now)).to_date
+      else
+        dt = DateTime.parse(dt).to_datetime
+        dtend = DateTime.parse( (DateTime.now + dt_end_offset(dt) ).to_date.to_s )
+      end
+
+      if text =~ /until ([[:alpha:]]{3} \d+)/
+        #puts "FF #{$1}"
+      end
+
+      if status != "GOOD SERVICE" and not douchebag name and not bus name
+        cal.events << RiCal.Event do
+          summary "#{name} #{status.titleize}"
+          dtstart     dt
+          dtend       dtend
+          location    name
+          description text
+
+          if not status =~ /Planned|Good/i
+            alarm do
+              description "#{name} #{status.titleize}"
+            end
+          end
+
+        end
+      end
+    end
+    return cal
+  end
 end
-
-puts cal
